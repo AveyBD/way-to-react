a5c8d5dbd1bbdc7e3c6f958a493566856df89f80
Debashish Das <talk@ddeb.me>
2020-11-09 00:38:50 +0600

    Added Props in Functional Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from "./myComponents/Hello";
+import Welcome from "./myComponents/Welcome";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hello name="Deb" age="26"/>
+      <Hello name="Das"/>
+        <Hello name="Avey"/>
+      <Welcome/>
     </div>
   );
 }
